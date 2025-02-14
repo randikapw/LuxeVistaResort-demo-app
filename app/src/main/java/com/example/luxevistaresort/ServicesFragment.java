713660c6d6ba4e3444dbe2c3041174cf08966a41
Randika Weerasooriya <randikapw@gmail.com>
2025-02-15 00:02:09 +0530

    Services back

--- app/src/main/java/com/example/luxevistaresort/ServicesFragment.java
+++ app/src/main/java/com/example/luxevistaresort/ServicesFragment.java
@@ -1,19 +1,18 @@
 package com.example.luxevistaresort;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.luxevistaresort.views.base.BaseFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ServicesFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ServicesFragment extends Fragment {
+public class ServicesFragment extends BaseFragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -57,7 +56,7 @@ public class ServicesFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                           Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_services, container, false);
     }
